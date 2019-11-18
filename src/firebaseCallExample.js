c28e2e4b49509ee99002333e5c5b19e70bfeb55c
James Ravenscroft <44760378+jamesravenscroft@users.noreply.github.com>
2019-11-17 17:12:23 -0800

    Update firebaseCallExample.js

--- src/firebaseCallExample.js
+++ src/firebaseCallExample.js
@@ -12,23 +12,23 @@ var config={
 firebase.initializeApp(config);
 var database= firebase.database();
 //clicky click variables
-var name;
-var destination;
-var morningTrain;
-var frequency = 0;
+var userName;
+var email;
+// var morningTrain;
+// var frequency = 0;
 //clicky function
-$("#add-train").on("click", function(){
+$("#add-user").on("click", function(){
     event.preventDefault();
-    name=$("#train-name").val().trim();
-    destination= $("#destination").val().trim();
-    morningTrain = $("#morning-train").val().trim();
-    frequency= $("#frequency").val().trim();
+    name=$("#user-name").val().trim();
+    email= $("#email").val().trim();
+//     morningTrain = $("#morning-train").val().trim();
+//     frequency= $("#frequency").val().trim();
 //addorama
 database.ref().push({
-    name:name,
-    destination: destination,
-    morningTrain: morningTrain,
-    frequency: frequency,
+    userName:userName,
+    email: email,
+//     morningTrain: morningTrain,
+//     frequency: frequency,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
 });
 $("form")[0].reset();
@@ -36,14 +36,14 @@ $("form")[0].reset();
 
 
 database.ref().on("child_added", function(childSnapshot){
-    var nextArrival;
-    var minsAway;
+    var nextRank;
+    var pointsAway;
     //append time
     var firstTrainNew = moment(childSnapshot.val().morningTrain, "hh:mm").subtract(1, "years");
     //difference between current and  morningTrain
     var diffTime= moment().diff(moment(firstTrainNew), "minutes");
     var remainder = diffTime % childSnapshot.val().frequency;
-    var minsAway = childSnapshot.val().frequency - remainder;
+    var pointsAway = childSnapshot.val().frequency - remainder;
 var nextTrain = moment().add(minsAway, "minutes");
 nextTrain= moment(nextTrain).format("hh:mm");
 //display infomania
