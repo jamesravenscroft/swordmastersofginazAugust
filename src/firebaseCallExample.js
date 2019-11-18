85159b5634a7ef50f107bb8947673a9b9cc6b1c2
James Ravenscroft <44760378+jamesravenscroft@users.noreply.github.com>
2019-11-17 17:27:39 -0800

    Update firebaseCallExample.js

--- src/firebaseCallExample.js
+++ src/firebaseCallExample.js
@@ -14,21 +14,21 @@ var database= firebase.database();
 //clicky click variables
 var userName;
 var email;
-// var morningTrain;
-// var frequency = 0;
+var points;
+var rank = 0;
 //clicky function
 $("#add-user").on("click", function(){
     event.preventDefault();
     name=$("#user-name").val().trim();
     email= $("#email").val().trim();
-//     morningTrain = $("#morning-train").val().trim();
+    points = $("#points").val().trim();
 //     frequency= $("#frequency").val().trim();
 //addorama
 database.ref().push({
     userName:userName,
     email: email,
-//     morningTrain: morningTrain,
-//     frequency: frequency,
+    points: points,
+    rank: rank
     dateAdded: firebase.database.ServerValue.TIMESTAMP
 });
 $("form")[0].reset();
@@ -39,19 +39,19 @@ database.ref().on("child_added", function(childSnapshot){
     var nextRank;
     var pointsAway;
     //append time
-    var firstTrainNew = moment(childSnapshot.val().morningTrain, "hh:mm").subtract(1, "years");
+    var currentRank = moment(childSnapshot.val().email, "hh:mm").subtract(1, "years");
     //difference between current and  morningTrain
-    var diffTime= moment().diff(moment(firstTrainNew), "minutes");
+    var diffPoints= moment().diff(moment(currentRank), "minutes");
     var remainder = diffTime % childSnapshot.val().frequency;
     var pointsAway = childSnapshot.val().frequency - remainder;
-var nextTrain = moment().add(minsAway, "minutes");
-nextTrain= moment(nextTrain).format("hh:mm");
+var nextRank = moment().add(pointsAway, "minutes");
+next moment(nextRank).format("hh:mm");
 //display infomania
 $("#add-row").append("<tr><td>" + childSnapshot.val().name +
 "</td><td>" + childSnapshot.val().destination +
 "</td><td>" + childSnapshot.val().frequency +
-"</td><td>" + nextTrain + 
-"</td><td>" + minsAway + "</td></tr>");
+"</td><td>" + nextRank + 
+"</td><td>" + pointAway + "</td></tr>");
 
 // console log dem errors yo
 }, function(errorObject) {
